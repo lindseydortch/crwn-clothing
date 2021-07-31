770b139a1fccdf9a5a84293d106ea1e76483a8eb
lindseydortch <codewithlinds@gmail.com>
2021-07-31 05:06:52 -0500

    updating our app with collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './components/homepage/Homepage';
+import ShopPage from './components/shop/shop';
+
 
 // HATS PAge ROUTE EXAMPLE 
-const HatsPage = () => {
-  return <div>
-    <h1>HATS PAGE</h1>
-  </div>
-}
+// const HatsPage = () => {
+//   return <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// }
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
